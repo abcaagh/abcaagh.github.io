522d60e25c6c5c0ec3a8ad3f0e7ef1591c919163
Bekzhan <abcaazh@gmail.com>
2020-08-12 12:12:08 +0600

    adap

--- works/AngelsNiko/src/js/script.js
+++ works/AngelsNiko/src/js/script.js
@@ -1,10 +1,10 @@
 const scrollUp = document.querySelector('.scroll-to-up')
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 100){
+    if (window.scrollY > 100) {
         scrollUp.setAttribute('data-show', 'true')
     }
-    if(window.scrollY < 100){
+    if (window.scrollY < 100) {
         scrollUp.setAttribute('data-show', 'false')
     }
 })
@@ -14,4 +14,12 @@ scrollUp.addEventListener('click', () => {
         behavior: 'smooth',
         top: 0
     })
+})
+//mobile-menu-toggle
+const menuToggle = document.querySelector('.menu-toggle')
+const menu = document.querySelector('.menu')
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('mobile-menu')
 })
